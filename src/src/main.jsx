00f39edf63818be4ddc42d82949fc05b65d988fa
orratc1-cell <orratc1@icloud.com>
2026-05-17 00:25:42 -0400

    Update main.jsx

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../villas-paraiso.jsx'
+import App from '../../villas-paraiso.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
